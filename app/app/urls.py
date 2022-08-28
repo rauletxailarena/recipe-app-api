17e4912147ee0e8739b5b5a7a38d0936430db4cc
Raul Ruiz <raul.ruiz@sngular.com>
2022-08-28 17:33:17 +0200

    Add two types of filters and foreign keys searches

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/recipe/', include('recipe.urls')),
+    path('reader/', include('reader.urls')),
 ]
--- app/reader/admin.py
+++ app/reader/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from reader.models import Reader
+
+from reader.models import Reader, Company
+
 # Register your models here.
 
 admin.site.register(Reader)
+admin.site.register(Company)
